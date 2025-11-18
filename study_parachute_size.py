a7daa2f7e47eb7abd66323ba2067d881ebd7e82d
dpress23 <dpress23@tamu.edu>
2025-11-17 20:20:48 -0600

    Update study_parachute_size.py
    Finalized File

--- study_parachute_size.py
+++ study_parachute_size.py
@@ -1,76 +1,88 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
-sys.path.append('P3_students')
+'''
+MEEN 357 Group 22 Phase 3 study_parachute_size 
 
-
+We are trying to model the termination time, rover speed
+at termination, and whether or not the mission was successful
+at a variety of parachute diameters (14m - 19m: step size = 0.5 m)
+'''
+# Import Mission Parameters
 from define_edl_system import *
 from subfunctions_EDL import *
 from define_planet import *
 from define_mission_events import *
 from redefine_edl_system import *
 
-# define the edl, planet, and mission events
+# Define the system parameters for the EDL,events, and planet.
 edl_system = define_edl_system_1()
+events = define_mission_events()
 mars = define_planet()
-mission_events = define_mission_events()
 
-# set all the values of the edl
-edl_system['altitude'] = 11000    # [m] initial altitude
-edl_system['velocity'] = -590     # [m/s] initial velocity
-edl_system['parachute']['deployed'] = True   # our parachute is open
-edl_system['parachute']['ejected'] = False   # and still attached
-edl_system['rover']['on_ground'] = False # the rover has not yet landed
+# EDL Values
+edl_system['altitude'] = 11000  # (m) Default Initial Altitude
+edl_system['velocity'] = -590  # (m/s) Default Initial Velocity
+edl_system['parachute']['deployed'] = True  # We want an Open Parachute
+edl_system['parachute']['ejected'] = False  # Parachute is open but not detached yet
+edl_system['rover']['on_ground'] = False  # Rover is not
 tmax = 2000
 
-# create the parachute diameter array
-parachute_diam = np.arange(14, 19, 0.5)
+# Tests all parachutes with diameters between (14m and 19m; step size 0.5m)
+diameter_parachute = np.arange(14, 19, 0.5)
 
-# create the empty arrays for time, velocity, and success
-tfinal = np.zeros(len(parachute_diam))
-vfinal = np.zeros(len(parachute_diam))
-sfinal = np.zeros(len(parachute_diam))
+# Final Parameters List, Created and set to zero before the for loop
+time_f = np.zeros(len(diameter_parachute))
+velocity_f = np.zeros(len(diameter_parachute))
+success_f = np.zeros(len(diameter_parachute)) # Final parameter for sucess
 
-for i in range(len(parachute_diam)):
-    # run the redifining function and simulate rover
+#Loop that checks if edl system
+for i in range(len(diameter_parachute)): # runs through all different parachutes
+    # Re-builds the system based off the specific parachute diameter being tested
     edl_system = redefine_edl_system(edl_system)
-    edl_system['parachute']['diameter'] = parachute_diam[i]
-    
-    [t,Y, edl_system] = simulate_edl(edl_system, mars, mission_events, tmax, True)
-    
-    # update final times with the last time value
-    tfinal[i] = t[-1]
-    
-    # update the final velocity by accessing it
-    vfinal[i] = edl_system['velocity']
-    
-    # check the conditions of success and then attribute value
-    if(edl_system['velocity'] <= 1.0 and edl_system['altitude'] >= 4.5):
-        sfinal[i] = 1.0
+    edl_system['parachute']['diameter'] = diameter_parachute[i]
+    [t, Y, edl_system] = simulate_edl(edl_system, mars, events, tmax, True)
+    time_f[i] = t[-1] # Final Time after run
+    velocity_f[i] = edl_system['velocity'] # Final Velocity after run
+
+# Success Check
+    if (edl_system['velocity'] <= 1.0 and edl_system['altitude'] >= 4.5): # Velocity must be <= 1 m/s and altitude must be >= 4.5 m
+        success_f[i] = 1.0 # run is successful :) !!!
     else:
-        sfinal[i] = 0.0
-    
-plt.figure(figsize=(10,10))
+        success_f[i]= 0.0 # run isn't successful :(
 
+# Formatting
+plt.figure(figsize=(10, 10))
+plt.suptitle('EDL Run Performance Over Varying Parachute Sizes',fontsize=16, fontweight='bold')
+
+# Parachute Diameter vs Time
 plt.subplot(3, 1, 1)
-plt.xlabel('Parachute Diameter [m]')
-plt.ylabel('Time [s]')
-plt.title('EDL Performance vs Parachute Size')
-plt.plot(parachute_diam, tfinal, linestyle = '--')
+plt.xlabel('Diameter (m)')
+plt.ylabel('Time (s)')
+plt.title('Parachute Diameter vs Time')
+plt.plot(diameter_parachute, time_f, linestyle='--', marker='.',)
 
+# Parachute Diameter vs Touchdown Speed'
 plt.subplot(3, 1, 2)
-plt.xlabel('Parachute Diameter [m]')
-plt.ylabel('Touchdown Speed [m/s]')
-plt.plot(parachute_diam, vfinal, linestyle = '--')
+plt.xlabel('Diameter (m)')
+plt.ylabel('Speed (m/s)')
+plt.title('Parachute Diameter vs Touchdown Speed')
+plt.plot(diameter_parachute, velocity_f, linestyle='--', marker='.',)
 
+# Parachute Diameter vs Landing Success
 plt.subplot(3, 1, 3)
-plt.xlabel('Parachute Diameter [m]')
-plt.ylabel('Landing Success [T/F]')
-plt.plot(parachute_diam, sfinal, linestyle = '--', marker = '.')
+plt.xlabel('Diameter (m)')
+plt.ylabel('Success (T/F])')
+plt.title('Parachute Diameter vs Landing Success')
+plt.plot(diameter_parachute, success_f, linestyle='--', marker='.',)
 
-plt.subplots_adjust(hspace=0.5)
+plt.subplots_adjust(hspace=0.5) ##  Corresponds with step size
+
+plt.show()
+
 
     
+
 
 
 
